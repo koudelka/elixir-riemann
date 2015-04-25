43fb5b63dc6bbe688c9afa6169e94a7361783f0c
Michael Shapiro <koudelka@ryoukai.org>
2015-04-25 16:28:19 -0700

    Adding some docs

--- lib/riemann.ex
+++ lib/riemann.ex
@@ -4,6 +4,11 @@ defmodule Riemann do
   alias Riemann.Proto.Msg
   alias Riemann.Proto.Event
 
+  @moduledoc """
+    A client for the Riemann event stream processor. http://riemann.io
+  """
+
+  @doc false
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
@@ -28,14 +33,33 @@ defmodule Riemann do
     Supervisor.start_link(children, opts)
   end
 
+  @doc """
+    Sends one or more events synchronously.
+
+    For example:
+    ``
+      Riemann.send(service: "my awesome app", metric: 5.0, attributes: [build: "7543"])
+
+      Riemann.send([
+        [service: "my awesome app req", metric: 1, attributes: [build: "7543"]],
+        [service: "things in queue", metric: 100, attributes: [build: "7543"]]
+      ])
+    ``
+  """
+  @spec send(nonempty_list) :: :ok
   def send(events) do
     do_send(events, :sync)
   end
 
+  @doc """
+    see `send/1`
+  """
+  @spec send(nonempty_list) :: :ok | {:error, atom}
   def send_async(events) do
     do_send(events, :async)
   end
 
+  @spec send(nonempty_list) :: :ok | {:error, atom}
   defp do_send(events, sync) do
     [events: Event.list_to_events(events)]
     |> Msg.new
